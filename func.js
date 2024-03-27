dd8128f4e1d0592c1c01852952580cfc66d10eca
git <ignaciosotelo8@gmail.com>
2024-03-27 20:17:22 -0300

    fusion componentes y estilos

--- func.js
+++ func.js
@@ -4,28 +4,23 @@ document.addEventListener("DOMContentLoaded", function() {
     const jardinLink = document.getElementById('jardin');
     const iluminacionLink = document.getElementById('iluminacion');
     const cocinaLink = document.getElementById('cocina');
-
     // Funciones para manejar los clics en los enlaces
     function manejarClicCuadros(event) {
         event.preventDefault(); // Evitamos que el enlace recargue la página
         alert('Ver cuadros"'); // Mostramos un mensaje (puedes cambiar esto según lo que desees hacer)
     }
-
     function manejarClicJardin(event) {
         event.preventDefault();
         alert('Ver arituclos de "Jardín y exterior"');
     }
-
     function manejarClicIluminacion(event) {
         event.preventDefault();
         alert('Ver articulos de "Iluminación"');
     }
-
     function manejarClicCocina(event) {
         event.preventDefault();
         alert('Ver elementos de "Cocina"');
     }
-
     // Agregamos event listeners a los enlaces para detectar clics
     cuadrosLink.addEventListener('click', manejarClicCuadros);
     jardinLink.addEventListener('click', manejarClicJardin);
